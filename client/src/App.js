b8af79f3410ca232cd10c435ca157fc2eb024c6e
Shivam Mishra <shivammishra15092003@gmail.com>
2023-10-11 12:08:32 +0530

    My first Commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+//components
+import Header from './components/header/header';
+function App() {
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
+}
+
+export default App;
